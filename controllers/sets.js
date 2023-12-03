09a98fb17ef01d030eaa7fc3a82c5137c7084cfe
BBrownley <brendobrownley@yahoo.com>
2023-12-02 19:20:36 -0500

    implement set creation functionality

--- controllers/sets.js
+++ controllers/sets.js
@@ -1,13 +1,5 @@
 /*
 
-Contains functions that interact with the database
-
-*/
-
-const dbConnection = require("../dbconnection").connection;
-
-/*
-
 Controller file for flash card sets
 
 */
@@ -19,7 +11,7 @@ const jwt = require("jsonwebtoken");
 
 const setsRouter = require("express").Router();
 
-//const usersModel = require("../models/users");
+const setsModel = require("../models/sets");
 
 /*
 
@@ -36,35 +28,53 @@ setsRouter.post("/", async (req, res, next) => {
   const payload = req.body;
   const userJWT = req.cookies.token.split(" ")[1];
 
-  console.log(payload);
-
   // 1) Validate - valid user JWT
 
-  let decoded;
+  let decodedUser;
 
   try {
-    decoded = jwt.verify(userJWT, process.env.JWT_SECRET);
+    decodedUser = jwt.verify(userJWT, process.env.JWT_SECRET);
   } catch (e) {
-    res
+    return res
       .status(422)
       .json({ error: "Unable to decode jsonwebtoken - token may be invalid or expired" });
   }
 
-  console.log(decoded);
-
   // Set insertion
 
-  // 2.1) Validate - title/desc fields are not empty
+  // 2.1) Validate - title fields are not empty, flash card fields filled in
 
-  // 2.2) Begin database transaction, insert sets
+  const setTitle = payload.title;
+  const setDesc = payload.description;
+  const cards = payload.cards;
 
-  // 2.3) Insert cards
+  if (setTitle.trim("").length === 0) {
+    return res.status(422).json({ error: "Set must be given a title" });
+  }
 
-  // Transaction status
+  try {
+    cards.forEach(card => {
+      const cardTerm = card.term;
+      const cardDefinition = card.definition;
 
-  // 3.1) Return server OK status if insertions successful, commit transaction
+      if (cardTerm.trim().length === 0 || cardDefinition.trim().length === 0) {
+        throw new Error("One or more cards are missing a term or a definition");
+      }
+    });
+  } catch (err) {
+    return res.status(422).json({ error: err.message });
+  }
 
-  // 3.2) Return server error if insertion unsuccessful, rollback transaction
+  // 2.2) Begin database transaction, insert sets and cards
+
+  try {
+    await setsModel.createSet(setTitle, setDesc, cards, decodedUser);
+  } catch (err) {
+    // Return server error if insertion unsuccessful, transaction rolled back
+    return res.status(422).json({ error: err.message });
+  }
+
+  return res.status(200).end();
 });
 
 module.exports = setsRouter;
---
+++ models/sets.js
@@ -0,0 +1,85 @@
+/*
+
+Contains functions that interact with the database (published_sets, published_cards table)
+
+*/
+
+const dbConnection = require("../dbconnection").connection;
+
+/*
+
+Creates a new entry in the published_sets table with corresponding cards in the published_cards table.
+
+*/
+
+const createSet = async (title, desc, cards, decodedUser) => {
+  const addSetQuery = `
+    INSERT INTO published_sets (id, name, description, author_id, skip_mastered_terms, mastery_requirement, created_at)
+    VALUES (?, ?, ?, ?, ?, ?, NOW())
+  `;
+
+  const addCardsQuery = `
+    INSERT INTO published_cards (id, set_id, term, definition, order_num, mastery_progress, created_at) 
+    VALUES ?
+  `;
+
+  try {
+    // using a transaction will allow us to roll back any changes made to published_sets if the
+    // addCardsQuery fails somehow. to do this, we must acquire a connection from the pool
+    dbConnection.getConnection((err, connection) => {
+      // Start the transaction
+      connection.beginTransaction(err => {
+        if (err) throw err;
+
+        connection.query(
+          addSetQuery,
+          [null, title, desc, decodedUser.id, false, 0],
+          (err, results1) => {
+            if (err) {
+              // Rollback the transaction in case of an error
+              return connection.rollback(() => {
+                throw err;
+              });
+            }
+
+            // set insert successful, get insert id to link with cards in published_cards table
+            const setInsertId = results1.insertId;
+
+            // transform card data into form corresponding to table columns
+            const cardInsertData = cards.map(card => {
+              return [null, setInsertId, card.term, card.definition, 0, 0, null];
+            });
+
+            connection.query(addCardsQuery, [cardInsertData], (err, results2) => {
+              if (err) {
+                // Rollback the transaction in case of an error
+                return connection.rollback(() => {
+                  throw err;
+                });
+              }
+
+              // Commit the transaction if all queries are successful
+              connection.commit(err => {
+                if (err) {
+                  // Rollback the transaction in case of an error during commit
+                  return connection.rollback(() => {
+                    throw err;
+                  });
+                }
+
+                // Release the connection back to the pool
+                connection.release();
+                console.log("Transaction completed successfully.");
+              });
+            });
+          }
+        );
+      });
+    });
+  } catch (err) {
+    console.error(`Error occurred while creating set: ${err.message}`, err);
+    throw new Error("Error occurred while creating set");
+  }
+};
+
+module.exports = { createSet };
